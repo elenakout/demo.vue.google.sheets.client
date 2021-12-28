3178d32f8c34f5e38ed272d9a69521ca00dc584f
elenakout <elenakout@gmail.com>
2021-12-28 18:19:21 +0200

    refactor: 🚧 create button component

---
+++ src/components/ButtonAddNote.vue
@@ -0,0 +1,35 @@
+<script>
+import { mapMutations } from 'vuex';
+
+export default {
+  name: 'ButtonAddNote',
+  components: {},
+  data() {
+    return {
+      note: {},
+    };
+  },
+  methods: {
+    ...mapMutations(['UPDATE_ACTIVE_NOTE']),
+    createNote() {
+      this.note = {
+        title: 'Title',
+        content: 'New note...',
+        category: 'note',
+        created_at: new Date(),
+        modified: new Date(),
+        id: null,
+      };
+
+      this.UPDATE_ACTIVE_NOTE(this.note);
+    },
+  },
+  computed: {},
+};
+</script>
+
+<template>
+  <button @click="createNote">Add Note</button>
+</template>
+
+<style scoped lang="scss"></style>
